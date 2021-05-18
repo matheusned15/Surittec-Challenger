3e34eb3841b247b74d635266244b3046675d99f0
matheusned15 <matheusned15@gmail.com>
2021-05-18 11:58:24 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/demo/SurittecProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurittecProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurittecProjectApplication.class, args);
+	}
+
+}
